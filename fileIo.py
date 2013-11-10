b32040fdb20d0529424e696e9f3e7d56954b2f9d
laisj <laisijia@gmail.com>
2013-11-10 14:07:41 +0800

    file and dir execute

---
+++ fileIo.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+def my_read_all(dir):
+        for files in os.listdir(dir):
+                filepath = os.path.join(dir, files)
+                if(os.path.isdir(filepath)):
+                        my_read_all(filepath)
+                        return
+                fp = open(filepath, "r")
+                print "### in file", filepath, " ###"
+                for line in fp:
+                        sys.stdout.write( "%s" % line)
+
+
+my_read_all("/home/lai/git/Toolkit")
