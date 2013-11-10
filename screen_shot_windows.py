4bd4d7d0f2dfc3a9eca1e1591f6627ec8a0275de
laisj <laisijia@gmail.com>
2013-11-10 15:07:40 +0800

    scirpts for screen shots on different platforms

---
+++ screen_shot_windows.py
@@ -0,0 +1,11 @@
+from PIL import ImageGrab
+import datetime
+import time
+
+timeStr = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+
+im = ImageGrab.grab()
+
+datetime.time()
+
+im.save(r'd:\\ScreenMonitor\\' + timeStr + '.jpg', 'jpeg')
