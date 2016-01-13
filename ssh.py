dea3400fe7fc01c8a6415bd28a3f645272b2ed5e
lai.sijia <lai.sijia@immomo.com>
2016-01-13 16:14:07 +0800

    fix window width problem

--- ssh.py
+++ ssh.py
@@ -5,6 +5,7 @@ import os
 print datetime.datetime.now()
 
 ssh = pexpect.spawn("ssh lai.sijia@gxxxxxx -p xxx")
+ssh.setwinsize(80, 160)
 ssh.expect("000*")
 ssh.sendline("python ssh.py")
 ssh.interact()
